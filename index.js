497304aa84f189f1f098f583cb58915a0a652e90
Semyon A. Zadorozhniy <semyon@codedesign.ru>
2024-03-19 11:12:58 +0300

    Added fileBook

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const { v4: uuid } = require('uuid')
-
+const router = express.Router()
+const fileMulter = require('./file')
 
 class Book {
-  constructor(id = uuid(), title = '', description = '', authors = '', favorite='', fileCover='', fileName='') {
+  constructor(
+    id = uuid(),
+    title = '',
+    description = '',
+    authors = '',
+    favorite = '',
+    fileCover = '',
+    fileName = '',
+    fileBook = ''
+  ) {
     this.id = id
     this.title = title || ''
     this.description = description || ''
@@ -11,33 +21,33 @@ class Book {
     this.favorite = favorite || ''
     this.fileCover = fileCover || ''
     this.fileName = fileName || ''
-    this.fileBook = fileBook || "";
+    this.fileBook = fileBook || ''
   }
 }
 
 const store = {
-  books: []
+  books: [],
 }
 
 const app = express()
 app.use(express.json())
 
 // Login
-app.post('/api/user/login', (req, res) => {
+router.post('/api/user/login', (req, res) => {
   res.status(201)
-  res.json({id:1, mail: 'test@mail.ru'})
+  res.json({ id: 1, mail: 'test@mail.ru' })
 })
 
 // Get list
-app.get('/api/books', (req, res) => {
+router.get('/api/books', (req, res) => {
   const { books } = store
   res.json(books)
 })
 
 // Get by id
-app.get('/api/books/:id', (req, res) => {
+router.get('/api/books/:id', (req, res) => {
   const { books } = store
-  const {id} = req.params
+  const { id } = req.params
   const currentBook = books.find(book => book.id === id)
 
   if (currentBook) {
@@ -49,11 +59,36 @@ app.get('/api/books/:id', (req, res) => {
 })
 
 // Create
-app.post('/api/books', (req, res) => {
+app.use('/api/books', fileMulter.single('fileBook'), (req, res, next) => {
+  if (req.file) {
+    const { path } = req.file
+    res.json({ path })
+  }
+  // res.json()
+  next()
+})
+
+router.post('/api/books', (req, res) => {
   const { books } = store
-  const { title, description, authors, favorite, fileCover, fileName } = req.body
   const id = uuid()
-  const newBook = new Book(id, title, description, authors, favorite, fileCover, fileName)
+
+  const book = {
+    id,
+    ...req.body,
+    fileBook: req.file ? req.file.path : '',
+    fileName: req.file ? req.file.originalname : '',
+  }
+
+  const newBook = new Book(
+    book.id,
+    book.title,
+    book.description,
+    book.authors,
+    book.favorite,
+    book.fileCover,
+    book.fileName,
+    book.fileBook
+  )
   books.push(newBook)
 
   res.status(201)
@@ -61,10 +96,10 @@ app.post('/api/books', (req, res) => {
 })
 
 // Update
-app.put('/api/books/:id', (req, res) => {
+router.put('/api/books/:id', (req, res) => {
   const { books } = store
-  const { title, description, authors, favorite, fileCover, fileName } = req.body
-  const {id} = req.params
+  const { title, description, authors, favorite, fileCover, fileName, fileBook } = req.body
+  const { id } = req.params
   const currentBook = books.filter(book => book.id === id)
 
   if (currentBook) {
@@ -77,33 +112,32 @@ app.put('/api/books/:id', (req, res) => {
           authors,
           favorite,
           fileCover,
-          fileName
+          fileName,
+          fileBook,
         }
       }
       return book
-      
     })
     res.json({
-        id,
-        title,
-        description,
-        authors,
-        favorite,
-        fileCover,
-        fileName
-      })
+      id,
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName,
+      fileBook,
+    })
   } else {
-      res.status(404)
-      res.json('Code:404 | Book was not found')
+    res.status(404)
+    res.json('Code:404 | Book was not found')
   }
-
-  
 })
 
 // Delete
-app.delete('/api/books/:id', (req, res) => {
+router.delete('/api/books/:id', (req, res) => {
   const { books } = store
-  const {id} = req.params
+  const { id } = req.params
   const currentBook = books.find(book => book.id === id)
 
   if (currentBook) {
@@ -114,8 +148,8 @@ app.delete('/api/books/:id', (req, res) => {
     res.status(404)
     res.json('Code:404 | Book was not found')
   }
-
 })
 
+app.use(router)
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto'
+};
