19d66f385e8eddb7701d16af0056be9436fcd1f3
Semyon Zadorozhniy <semyon.z@icloud.com>
2024-03-26 02:24:16 +0300

    CRUD for book

--- Routes/booksRouter.js
+++ Routes/booksRouter.js
@@ -50,9 +50,78 @@ router.get('/', (req, res) => {
 })
 
 router.get('/create', (req, res) => {
-  res.render('books/create', { title: 'Create book' })
+  res.render('book/create', { title: 'Create book', todo: {} })
 })
 
-router.post('/create', (req, res) => {})
+router.post('/create', (req, res) => {
+  const { books } = store
+  const { title, description } = req.body
+
+  const newBook = new Book(uuid(), title, description)
+  books.push(newBook)
+
+  res.redirect('/books')
+})
+
+router.get('/view/:id', (req, res) => {
+  const { books } = store
+  const { id } = req.params
+  const book = books.find(book => book.id === id)
+
+  if (!book) {
+    res.status(404).send('404 Not Found')
+    return
+  } else {
+    res.render('book/view', { title: book.title, book })
+  }
+})
+
+router.get('/update/:id', (req, res) => {
+  const { books } = store
+  const { id } = req.params
+  const book = books.find(book => book.id === id)
+
+  if (!book) {
+    res.status(404).send('404 Not Found')
+    return
+  } else {
+    res.render('book/update', { title: book.title, book })
+  }
+})
+
+router.post('/update/:id', (req, res) => {
+  console.log('xxxxxxxxxxx');
+  const { books } = store
+  const { id } = req.params
+  const { title, description } = req.body
+
+  const book = books.find(book => book.id === id)
+
+  if (!book) {
+    res.status(404).send('404 Not Found')
+    return
+  } else {
+    book.title = title
+    book.description = description
+
+    res.redirect('/books')
+  }
+})
+
+router.get('/delete/:id', (req, res) => {
+  const { books } = store
+  const { id } = req.params
+
+  const bookIndex = books.findIndex(book => book.id === id)
+
+  if (bookIndex === -1) {
+    res.status(404).send('404 Not Found')
+    return
+  } else {
+    books.splice(bookIndex, 1)
+
+    res.redirect('/books')
+  }
+})
 
 module.exports = router
